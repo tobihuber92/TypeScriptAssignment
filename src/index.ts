26093c4a8c21e6f8d0984c77538b8a454a0b0933
Mysteriedev <josefsle97@gmail.com>
2021-03-16 11:35:10 +0100

    querySelector Examples

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,18 @@ console.log(helloWorld);
 customElements.define("my-beispiel", Beispiel);
 
 alertMe();
+
+const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
+
+const myInputValueAlternate = document.querySelector(
+  "#myInput"
+) as HTMLInputElement;
+
+document
+  .querySelector<HTMLInputElement>("#myInput")
+  ?.addEventListener("focus", doSmth);
+
+function doSmth(e: UIEvent) {
+  const val = e.target as HTMLInputElement;
+  console.log(e, val.value);
+}
