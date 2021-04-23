15713f84f829e69d6cdc97899da3a7440f3ce0b3
Tobias Huber <tobias.huber.oppenau@outlook.de>
2021-04-23 18:43:54 +0200

    added styling

--- src/myOtherModule.ts
+++
@@ -1,5 +0,0 @@
-// THIS IS A MODULE
-
-export function alertMe(): void {
-  alert("ATTENTION PLEASE");
-}
--- src/unusedModule.ts
+++
@@ -1,6 +0,0 @@
-//THIS MODULE WILL BE TREESHAKED CAUSE ITS FUNCTIONS ARENT USED
-
-//will not show up in the bundle
-function unused() {
-  return "hugh, why was this neccesary";
-}
