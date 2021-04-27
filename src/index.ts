c741124fe40a08af39bf7676b973ecbc92ec6c61
Tobias Huber <tobias.huber.oppenau@outlook.de>
2021-04-27 22:09:48 +0200

    sounds and click effects added

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,18 @@ let btnRed = document.querySelector('.quarterCircleRed');
 let btnYellow = document.querySelector('.quarterCircleYellow');
 let btnBlue = document.querySelector('.quarterCircleBlue');
 
-btnGreen?.addEventListener("click", function(){callButton("green")})
-btnRed?.addEventListener("click", function(){callButton("red")})
-btnYellow?.addEventListener("click", function(){callButton("yellow")})
-btnBlue?.addEventListener("click", function(){callButton("blue")})
+btnGreen?.addEventListener("click", function(){irgendwas("green")})
+btnRed?.addEventListener("click", function(){irgendwas("red")})
+btnYellow?.addEventListener("click", function(){irgendwas("yellow")})
+btnBlue?.addEventListener("click", function(){irgendwas("blue")})
+
+function irgendwas(farbe:string){
+    nextSequence();
+    callButton(farbe);
+    playAudio(farbe);
+    activateButton(farbe);
+}
+
 
 let allButtons = [btnGreen, btnRed, btnYellow, btnBlue]
 let sequence: any[] =[];
@@ -29,4 +37,36 @@ function callButton(farbe:string):void{
     console.log(userClickedPattern);
     nextSequence();
     //console.log(sequence);
+}
+
+function playAudio(farbe:string){
+    const audio = new Audio("../src/sounds/" + farbe + ".mp3")
+    audio.play()
+}
+
+function activateButton(farbe:string){
+    if(farbe==="green"){
+        btnGreen?.classList.add("active");
+        console.log("testgrün");
+    }
+    else if(farbe==="red"){
+        btnRed?.classList.add("active");
+        console.log("testrot");
+    }
+    else if(farbe==="yellow"){
+        btnYellow?.classList.add("active");
+    }
+    else if(farbe==="blue"){
+        btnBlue?.classList.add("active");
+    }
+
+setTimeout(deactivateButton, 300)
+}
+
+function deactivateButton(){
+    btnGreen?.classList.remove("active");
+    btnRed?.classList.remove("active");
+    btnYellow?.classList.remove("active");
+    btnBlue?.classList.remove("active");
+
 }
